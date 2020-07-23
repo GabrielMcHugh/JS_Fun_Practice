bf18280dec3ede1a5617fe1b888b83523a8016f1
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-07-23 12:47:40 +1000

    added maxb and add methods for generalised no. of arguments (for loop method and reduce method)

--- Solutions/my-solutions-GM.js
+++ Solutions/my-solutions-GM.js
@@ -32,6 +32,67 @@ console.log(
 	"mulb(2,1) returns:", mulb(2,1), "\n" +
 	"mulb(4,7) returns:", mulb(4,7))
 
+//Write a binary function minb that takes two numbers and returns the smaller one
+function minb(a,b) {
+	if (a < b) {
+		return a;
+	}
+	return b;
+}
+
+console.log(
+	"minb(2,1) returns:", minb(2,1), "\n" +
+	"minb(4,-7) returns:", minb(4,-7))
+
+//Write a binary function maxb that takes two numbers and returns the larger one
+const maxb = function assignedFunction(a,b) {
+	if (a > b) {
+		return a;
+	}
+	return b;
+}
+
+console.log(
+	"maxb(2,1) returns:", maxb(2,1), "\n" +
+	"maxb(4,-7) returns:", maxb(4,-7))
+
+//Write a function add that is generalized for any amount of arguments
+/* For loop method
+- the spread operator gathers remaining operators and turns them into an array
+*/
+const add = function assignedFunction(...nums) {
+	for (i = 0, sum = 0; i < nums.length; i++) {
+		sum += nums[i];
+	}
+	return sum;
+}
+console.log("add(3,9,8,12) returns: ", add(3,9,8,12))
+
+/* Reduce method
+- I didn't simplify it because I was trying to experiment and understand how the reduce
+method worked
+*/
+const add = function assignedFunction(...nums) {
+	initialValue = 0;
+	return nums.reduce(function reducer(accumulator, item) {
+		return accumulator + item;
+	}, initialValue)
+}
+console.log("add(3,9,8,12) returns: ", add(3,9,8,12))
+
+/* Slight cleaner reduce method
+- to make it more obvious what I did
+*/
+const add = function assignedFunction(...nums) {
+	initialValue = 0;
+
+	const reducer = function reducer(accumulator, item) {return accumulator + item}
+
+	return nums.reduce(reducer, initialValue)
+}
+console.log("add(3,9,8,12) returns: ", add(3,9,8,12))
+
+
 //Write a function max that is generalised for any amount of arguments
 
 const max = (...nums) => {return nums.reduce((a,b) => a > b ? a : b);}
